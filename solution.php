aa4e958dc03ecdeb14460f36db06095e3d6b1446
Bishnu pokhrel <43365095+Bishnupkl@users.noreply.github.com>
2024-09-30 15:09:27 -0500

    Create solution.php
    submitted answer

---
+++ solution.php
@@ -0,0 +1,23 @@
+<?
+function twoSum($nums, $target) {
+    $map = []; // Key: number, Value: index
+
+    for ($i = 0; $i < count($nums); $i++) {
+        $complement = $target - $nums[$i];
+        
+        if (isset($map[$complement])) {
+            return [$map[$complement], $i];
+        }
+        
+        $map[$nums[$i]] = $i;
+    }
+
+    return []; // Return an empty array if no solution is found
+}
+
+// Example usage
+$nums = [2, 7, 11, 15];
+$target = 9;
+$result = twoSum($nums, $target);
+print_r($result); // Output: [0, 1]
+?>
