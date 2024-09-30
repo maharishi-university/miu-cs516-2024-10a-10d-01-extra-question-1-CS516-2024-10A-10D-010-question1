be79d03934acb55be9cc2d60b1fb7221f6cbf09d
Bishnu pokhrel <43365095+Bishnupkl@users.noreply.github.com>
2024-09-30 15:15:29 -0500

    Update solution.php

--- solution.php
+++ solution.php
@@ -1,5 +1,16 @@
 <?
 function twoSum($nums, $target) {
+    $map = [];
+
+
+
+    for ($i = 0; $i < count($nums); $i++) {
+        $complement = $target - $nums[$i];
+        
+        if (isset($map[$complement])) {
+            return [$map[$complement], $i];
+        }<?
+function twoSum($nums, $target) {
     $map = []; // Key: number, Value: index
 
     for ($i = 0; $i < count($nums); $i++) {
@@ -20,4 +31,22 @@ $nums = [2, 7, 11, 15];
 $target = 9;
 $result = twoSum($nums, $target);
 print_r($result); // Output: [0, 1]
+?>
+
+        
+        $map[$nums[$i]] = $i;
+    }
+
+
+
+    return []; 
+}
+
+
+
+
+$nums = [2, 7, 11, 15];
+$target = 9;
+$result = twoSum($nums, $target);
+print_r($result);
 ?>
